6cb219e4a8dd6d097d609014f24a3c582c7db6e2
Your Name <you@example.com>
2022-01-06 21:53:50 +0400

    1

---
+++ app/src/main/java/com/example/my_popular_garage/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.my_popular_garage
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.very_first_page)
+    }
+}
